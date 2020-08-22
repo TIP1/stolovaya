e028a48f704fade848f04c4f7d86b6cf85d814c7
TIP1 <tip219@bk.ru>
2020-08-22 14:36:12 +0300

    main complete

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 var swiper = new Swiper('.swiper-container', {
-    cssMode: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination'
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
