a0430cb6a84353e2ddef708ac9a5d1cc72c244e0
TIP1 <tip219@bk.ru>
2020-09-01 22:59:22 +0300

    start to doing mobile

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ var swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination',
   },
 });
+
+
+var header_nav = document.querySelector('.header-search-eq');
+console.log(header_nav.style.top);
+
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+var st = $(this).scrollTop();
+if (st > lastScrollTop){
+   console.log(1)
+} else {
+   console.log(2)
+}
+lastScrollTop = st;
+});
